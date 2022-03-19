346ca1a559fc440bf3d6afa1d6e5d6b71bb512eb
iamadilanwar <iamadilanwar@gmail.com>
2022-03-19 13:43:27 +0530

    mongodb

--- helpers/init_mongodb.js
+++ helpers/init_mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect("mongodb+srv://adil_anwar:TjWFgwZ1amTbhBhw@cluster0.rlu65.mongodb.net/dashboard1?retryWrites=true&w=majority", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
