f98303b346d37ee832b04c963d866cc1cea2a48d
iamadilanwar <iamadilanwar@gmail.com>
2022-04-10 16:12:36 +0530

    cros added

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -8,7 +8,7 @@ module.exports = {
       const payload = {};
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: "250s",
+        expiresIn: "60s",
         issuer: "adilanwar.me",
         audience: userId,
       };
--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-const express = require('express')
-const morgan = require('morgan')
-const createError = require('http-errors')
-require('dotenv').config()
-require('./helpers/init_mongodb')
-require('./helpers/init_redis')
+const express = require("express");
+const morgan = require("morgan");
+var cors = require("cors");
+const createError = require("http-errors");
+require("dotenv").config();
+require("./helpers/init_mongodb");
+require("./helpers/init_redis");
 
-const AuthRoute = require('./Routes/Auth.route')
-const UserRoute = require('./Routes/User.route')
-const CardRoute = require('./Routes/Card.route')
-const SubCardsRoute = require('./Routes/SubCards.route')
-const HistoryRoute = require('./Routes/History.route')
-const MobileRoute = require('./Routes/Mobile.route')
+const AuthRoute = require("./Routes/Auth.route");
+const UserRoute = require("./Routes/User.route");
+const CardRoute = require("./Routes/Card.route");
+const SubCardsRoute = require("./Routes/SubCards.route");
+const HistoryRoute = require("./Routes/History.route");
+const MobileRoute = require("./Routes/Mobile.route");
 
-const app = express()
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/mobile', MobileRoute)
-app.use('/auth', AuthRoute)
-app.use('/user', UserRoute)
-app.use('/card', CardRoute)
-app.use('/allSubCards', SubCardsRoute)
-app.use('/history', HistoryRoute)
-
+app.use("/mobile", MobileRoute);
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
+app.use("/card", CardRoute);
+app.use("/allSubCards", SubCardsRoute);
+app.use("/history", HistoryRoute);
 
 app.use(async (req, res, next) => {
-  next(createError.NotFound())
-})
+  next(createError.NotFound());
+});
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500)
+  res.status(err.status || 500);
   res.send({
     error: {
       status: err.status || 500,
       message: err.message,
     },
-  })
-})
-app.get('/', (req, res) => { res.send('Hello World!') })
+  });
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
