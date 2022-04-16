15c643553955450870ac3b077b0608b9346c052d
iamadilanwar <iamadilanwar@gmail.com>
2022-04-16 12:20:50 +0530

    file upload

--- Controllers/File.Controller.js
+++ Controllers/File.Controller.js
@@ -1,9 +1,11 @@
 const API_URL = process.env.API_URL;
 
 module.exports = {
-    uploadFile: async (req, res, next) => {
+  uploadFile: async (req, res, next) => {
     try {
       const uploadedFile = req.file;
+      const host = req.host;
+
       if (uploadedFile === null || uploadedFile === undefined) {
         return res
           .json({
@@ -13,10 +15,11 @@ module.exports = {
           .status(400);
       }
       const imageUrl = uploadedFile.path;
+      const filePath = req.protocol + "://" + host + "/" + req.file.path;
       res.json({
         error: false,
         msg: "OK",
-        url: { base: API_URL, path: imageUrl },
+        url: { base: API_URL, path: imageUrl, filePath: filePath },
       });
     } catch (error) {
       console.log("whbfhiwevhwevhwe");
